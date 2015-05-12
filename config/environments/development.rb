0b0b95c98790f49c374b0f84539f74f39a93de83
Dion <kalani.pettaway@gmail.com>
2015-05-12 12:53:57 -0400

    Mailing List no deploy

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,7 +29,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: Rails.application.secrets.domain_name, authentication: "plain",
+    domain: Rails.application.secrets.domain_name,
+    authentication: "plain",
     enable_starttls_auto: true,
     user_name: Rails.application.secrets.email_provider_username, 
     password: Rails.application.secrets.email_provider_password
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :contacts, only: [:new, :create]
-  root to: 'visitors#new'
+  resources :visitors, only: [:new, :create]
+
+  root 'visitors#new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
